7fe8a6988b378f53c076e4ffd6eef2f60283aaf1
kayoo-glitch <kay.oo@student.nhcs.net>
2026-09-21 22:47:48 -0400

    Update form-validation.js

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -1,1 +1,32 @@
+function checkMissing() {
+    
+    var emptyField = "";
 
+    if (document.getElementById('fullName').value == "" && document.getElementById('email').value == "" && document.getElementById('writeDown').value == "" ) {
+        emptyField += "You have 3 input that is incomplete";
+    }
+
+    if (document.getElementById('fullName').value == "" && document.getElementById('writeDown').value == "") {
+        emptyField += "You have 2 input that is incomplete";
+    }
+    if (document.getElementById('fullName').value == "" && document.getElementById('email').value == "") {
+        emptyField += "You have 2 input that is incomplete";
+    }
+    if (document.getElementById('email').value == "" && document.getElementById('writeDown').value == "") {
+        emptyField += "You have 2 input that is incomplete";
+    }
+
+    if (document.getElementById('fullName').value == "") {
+        emptyField += "You have 1 input that is incomplete";
+    }
+    if (document.getElementById('email').value == "") {
+        emptyField += "You have 1 input that is incomplete";
+    }
+    if (document.getElementById('writeDown').value == "") {
+        emptyField += "You have 1 input that is incomplete";
+    }
+
+}
+function validateEmail() {
+
+}
