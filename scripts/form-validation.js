12229d856f2654024393ef001925a6e52a1e5e15
kayoo-glitch <kay.oo@student.nhcs.net>
2026-09-22 22:41:37 -0400

    Update form-validation.js

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -3,6 +3,9 @@ Name: Kay Nae Oo
 Date: 9/22/2026
 */
 
+/*
+This function will tell you how many amount of your input is incomplete.
+*/
 function checkMissing() {
     
     var emptyField = "";
@@ -31,9 +34,15 @@ function checkMissing() {
         emptyField += "You have 1 input that is incomplete";
     }
 }
+
+/*
+This reads how many character you typed.
+*/
 const emailInput = document.getElementById('email')
 Input.addEventListener('email', validateEmail)
-
+/*
+This function will tell you if the email input has 8 character or not.
+*/
 function validateEmail() {
     const emailLength = input.value.length
     const minimumLength = 8
@@ -45,6 +54,9 @@ function validateEmail() {
         response += "The email input has to have at least 8 characters."
     }
 }
+/*
+This function will alert only if input is empty.
+*/
 function alert() {
     if (document.getElementById('fullName').value == "" || document.getElementById('email').value == "" || document.getElementById('writeDown').value == "") {
         alert("If form submission is blocked Its due to incomplete or invalid fields.")
